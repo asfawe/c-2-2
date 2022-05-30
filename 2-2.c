b4e4a12da97b6ac0a3922a406bf2b3db38b8ad29
LeeHack <aronia3006@gmail.com>
2022-05-30 11:38:33 +0900

    first commit

---
+++ 2-2.c
@@ -0,0 +1,17 @@
+// 2-2.c
+// 변수 여러개 한줄에  사용하기. 
+#include<stdio.h>
+
+int main(){
+
+	int num1, num2, num3;
+	
+	num1 = 10;
+	num2 = 20;
+	num3 = 30;
+	
+	printf("%d %d %d", num1, num2, num3);
+	
+	return 0;
+
+}
